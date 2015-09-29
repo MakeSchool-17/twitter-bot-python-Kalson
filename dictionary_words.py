99ba47829d583b98dd86cdfc12a0249306b34db0
Kalson Kalu <Kalson.Kalu@yahoo.com>
2015-09-28 22:40:13 -0700

    Random Dictionary Words

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,5 +1,14 @@
-with open('/usr/share/dict/words') as openfile:
-    for line in openfile:
-        for part in line.split():
-            if __name__ == '__main__':
-                print(part)
+import random
+from sys import argv
+
+content = open('/usr/share/dict/words').read().split()
+openfile_list = []
+for words in content:
+    openfile_list.append(words)
+sentence = []
+while len(sentence) < int(argv[1]):
+        sentence.append(openfile_list[random.randint(0, len(openfile_list))])
+        print(*sentence)
+    # argv - method for the command line with the index of
+    # 1 so a valid argument can be passed,
+    # instead of an index of 0
